d21d4a1962f4af83cb7efabfff1ad8b7844e740a
Romarioagrow <Romarioagrow@mail.ru>
2019-04-19 21:56:49 +0300

    v5.2 Ajax фильтры; универсальное js наполнение

--- src/main/resources/static/js/filter.js
+++ src/main/resources/static/js/filter.js
@@ -5,67 +5,21 @@ $(document).ready(function(){
 function collectFilters(e) {
     e.preventDefault();
 
-    /// методы наполнения переменных взависимости от типа товара
-    /// function collectTvFilters, collectFridgesFilters и тд
-    var sortBy          = [];
-    var sortmin         = [];
-    var sortmax         = [];
-    var brand           = [];
-    var country         = [];
-    var diag_min        = [];
-    var diag_max        = [];
-    var tv_resolution   = [];
-    var tv_params       = [];
-
-    sortBy.push     ($('input[name="sort_options"]:checked').val());
-    sortmin.push    ($('#sortmin').val());
-    sortmax.push    ($('#sortmax').val());
-    brand.push      ($('#brand').val());
-    country.push    ($('#country').val());
-    diag_min.push   ($('#diag_min').val());
-    diag_max.push   ($('#diag_max').val());
-
-    $(document.getElementsByName('tv_resolution')).each(function() {
-        if ($(this).is(':checked')) {
-            tv_resolution.push($(this).val());
-        }
-    });
-    $(document.getElementsByName('tv_params')).each(function() {
-        if ($(this).is(':checked')) {
-            tv_params.push($(this).val());
-        }
-    });
-
-    var tv_data = {
-        'sortBy':sortBy,
-        'sortmin':sortmin, 'sortmax': sortmax,
-        'brands': brand, 'country': country,
-        'diag_min': diag_min, 'diag_max': diag_max,
-        'resolution': tv_resolution, 'params': tv_params
-    };
-    console.log(tv_data);
-
-    if (tv_data.sortmin[0]  === '') delete tv_data.sortmin;
-    if (tv_data.sortmax[0]  === '') delete tv_data.sortmax;
-    if (tv_data.brands[0]   === '') delete tv_data.brands;
-    if (tv_data.country[0]  === '') delete tv_data.country;
-    if (tv_data.diag_min[0] === '' || tv_data.diag_min[0] === undefined) delete tv_data.diag_min;
-    if (tv_data.diag_max[0] === '' || tv_data.diag_max[0] === undefined) delete tv_data.diag_max;
-    if (tv_data.resolution.length === 0) delete tv_data.resolution;
-    if (tv_data.params.length     === 0) delete tv_data.params;
-    console.log(tv_data);
-
-    tv_data = JSON.stringify(tv_data);
-    console.log(tv_data);
-
     var url = document.URL;
     console.log(url);
+
+    var filters = constructFiltersData(url);
+    console.log(filters);
+
+    deleteNullParams(filters);
+
+    filters = JSON.stringify(filters);
 
     $.ajax({
         url: url,
         type: 'POST',
         dataType: 'json',
-        data: tv_data,
+        data: filters,
         processData: false,
         async: true,
         headers: {'Content-Type': 'application/json'},
@@ -126,3 +80,49 @@ function collectFilters(e) {
         }
     });
 }
+
+function constructFiltersData(url) {
+    if (url.includes("tv")) return collectTvFilters();
+}
+
+function collectTvFilters() {
+    var sortBy          = [($('input[name="sort_options"]:checked').val())];
+    var sortmin         = [($('#sortmin').val())];
+    var sortmax         = [($('#sortmax').val())];
+    var brand           = [($('#brand').val())];
+    var country         = [($('#country').val())];
+    var diag_min        = [($('#diag_min').val())];
+    var diag_max        = [($('#diag_max').val())];
+    var tv_resolution   = [];
+    var tv_params       = [];
+
+    $(document.getElementsByName('tv_resolution')).each(function() {
+        if ($(this).is(':checked')) {
+            tv_resolution.push($(this).val());
+        }
+    });
+    $(document.getElementsByName('tv_params')).each(function() {
+        if ($(this).is(':checked')) {
+            tv_params.push($(this).val());
+        }
+    });
+
+    return {
+        'sortBy': sortBy,
+        'sortmin': sortmin, 'sortmax': sortmax,
+        'brands': brand, 'country': country,
+        'diag_min': diag_min, 'diag_max': diag_max,
+        'resolution': tv_resolution, 'params': tv_params
+    };
+}
+
+function deleteNullParams(filters) {
+    for (var key in filters) {
+        if (filters[key][0] === '') {
+            delete filters[key];
+        }
+    }
+    console.log(filters);
+}
+
+
